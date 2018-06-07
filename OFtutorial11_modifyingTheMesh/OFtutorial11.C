a58736940c1f4fe568c4bd4d6604285b5a7ddb2e
UnnamedMoose <arturli@vp.pl>
2018-06-07 13:51:37 +0100

    Finished tutorial 11

--- OFtutorial11_modifyingTheMesh/OFtutorial11.C
+++ OFtutorial11_modifyingTheMesh/OFtutorial11.C
@@ -53,8 +53,8 @@ int main(int argc, char *argv[])
     Info << "Created cell types" << endl;
 
     // create a point cloud between which the cells will stretch
-    pointField points(12);
-    points[ 0] = vector(0.0, 0, 0);
+    pointField points(17);
+    points[ 0] = vector(0.0, 0, 0); // hexes
     points[ 1] = vector(0.5, 0, 0);
     points[ 2] = vector(1.0, 0, 0);
     points[ 3] = vector(0.0, 1, 0);
@@ -66,32 +66,63 @@ int main(int argc, char *argv[])
     points[ 9] = vector(0.0, 1, 0.1);
     points[10] = vector(0.5, 1, 0.1);
     points[11] = vector(1.0, 1, 0.1);
+    points[12] = vector(0.7, 0.5, -0.5); // tip of pyramid
+    points[13] = vector(0.7, 1.0, -0.5); // last point of a tet
+    points[14] = vector(0.7, 1.2, -0.5); // top face of a prism
+    points[15] = vector(0.5, 1.2, 0.0);
+    points[16] = vector(1.0, 1.2, 0.0);
     Info << "Created points" << endl;
 
     // create the cells from a particular cell shape and list of points
-    // TODO check if point ordering is important or not
+    // ordering is important
     List<cellShape> cells;
 
     List<label> cellPoints(8);
-
-    cellPoints[0] = 0;
+    cellPoints[0] = 0; // first face - x=0.0, clock-wise loop looking outwards
     cellPoints[1] = 3;
     cellPoints[2] = 9;
     cellPoints[3] = 6;
-    cellPoints[4] = 1;
+    cellPoints[4] = 1; // second face - x=0.5, points 4-7 share edges with 0-3, respectively
     cellPoints[5] = 4;
     cellPoints[6] = 10;
     cellPoints[7] = 7;
     cells.append(cellShape(hex, cellPoints));
-    cellPoints[0] = 1;
-    cellPoints[1] = 4;
-    cellPoints[2] = 10;
-    cellPoints[3] = 7;
-    cellPoints[4] = 2;
-    cellPoints[5] = 5;
-    cellPoints[6] = 11;
-    cellPoints[7] = 8;
+
+    cellPoints[0] = 4;
+    cellPoints[1] = 10;
+    cellPoints[2] = 7;
+    cellPoints[3] = 1;
+    cellPoints[4] = 5;
+    cellPoints[5] = 11;
+    cellPoints[6] = 8;
+    cellPoints[7] = 2;
     cells.append(cellShape(hex, cellPoints));
+
+    cellPoints.resize(6);
+    cellPoints[0] = 13;
+    cellPoints[1] = 4;
+    cellPoints[2] = 5;
+    cellPoints[3] = 14;
+    cellPoints[4] = 15;
+    cellPoints[5] = 16;
+    cells.append(cellShape(prism, cellPoints));
+
+    // ordering of pyramid and tet points seems to be random
+    cellPoints.resize(5);
+    cellPoints[0] = 4;
+    cellPoints[1] = 5;
+    cellPoints[2] = 2;
+    cellPoints[3] = 1;
+    cellPoints[4] = 12;
+    cells.append(cellShape(pyr, cellPoints));
+
+    cellPoints.resize(4);
+    cellPoints[0] = 12;
+    cellPoints[1] = 4;
+    cellPoints[2] = 5;
+    cellPoints[3] = 13;
+    cells.append(cellShape(tet, cellPoints));
+
     Info << "Created cells" << endl;
 
     // create patch definitions from lists of points defining individual faces
@@ -101,32 +132,95 @@ int main(int argc, char *argv[])
     // a single container.
     faceListList patchFaces;
 
+    // create a single face
     List<label> patchPoints(4);
-
-    patchPoints[0] = 3;
+    patchPoints[0] = 3; // ordered clock-wise looking outside of fluid domain
     patchPoints[1] = 9;
-    patchPoints[2] = 10;
-    patchPoints[3] = 4;
+    patchPoints[2] = 6;
+    patchPoints[3] = 0;
     patchFaces.append(List<face>(1, face(patchPoints)));
+
+    // create a list of faces constituting one patch
+    List<face> faces(4);
+    patchPoints[0] = 14;
+    patchPoints[1] = 15;
+    patchPoints[2] = 4;
+    patchPoints[3] = 13;
+    faces[0] = face(patchPoints);
+    patchPoints[0] = 15;
+    patchPoints[1] = 16;
+    patchPoints[2] = 5;
+    patchPoints[3] = 4;
+    faces[1] = face(patchPoints);
+    patchPoints[0] = 16;
+    patchPoints[1] = 14;
+    patchPoints[2] = 13;
+    patchPoints[3] = 5;
+    faces[2] = face(patchPoints);
+    patchPoints.resize(3);
+    patchPoints[0] = 15;
+    patchPoints[1] = 14;
+    patchPoints[2] = 16;
+    faces[3] = face(patchPoints);
+    patchFaces.append(faces);
+
+    // add external faces of the tet
+    faces.resize(2);
+    patchPoints[0] = 4;
+    patchPoints[1] = 12;
+    patchPoints[2] = 13;
+    faces[0] = face(patchPoints);
+    patchPoints[0] = 13;
+    patchPoints[1] = 12;
+    patchPoints[2] = 5;
+    faces[1] = face(patchPoints);
+    patchFaces.append(faces);
+
+    // add external faces of the pyramid
+    faces.resize(3);
+    patchPoints[0] = 4;
+    patchPoints[1] = 1;
+    patchPoints[2] = 12;
+    faces[0] = face(patchPoints);
+    patchPoints[0] = 12;
+    patchPoints[1] = 1;
+    patchPoints[2] = 2;
+    faces[1] = face(patchPoints);
+    patchPoints[0] = 12;
+    patchPoints[1] = 2;
+    patchPoints[2] = 5;
+    faces[2] = face(patchPoints);
+    patchFaces.append(faces);
+
     Info << "Created patches" << endl;
 
     // patch names
     List<word> boundaryPatchNames;
-    boundaryPatchNames.append("side0");
+    boundaryPatchNames.append("hexSide0");
+    boundaryPatchNames.append("prismFaces");
+    boundaryPatchNames.append("tetFaces");
+    boundaryPatchNames.append("pyramidFaces");
 
     // types and physical types
-    // TODO figure out how these are supposed to look really, now just use defaults
-    wordList boundaryPatchTypes(patchFaces.size(), polyPatch::typeName);
-    wordList boundaryPatchPhysicalTypes
-    (
-        patchFaces.size(),
-        polyPatch::typeName
-    );
+    wordList boundaryPatchTypes(patchFaces.size());
+    boundaryPatchTypes[0] = "symmetryPlane";
+    boundaryPatchTypes[1] = "patch";
+    boundaryPatchTypes[2] = "wall";
+    boundaryPatchTypes[3] = "symmetry";
 
-    // default values for other fields
+    wordList boundaryPatchPhysicalTypes(patchFaces.size());
+    boundaryPatchPhysicalTypes[0] = "symmetryPlane";
+    boundaryPatchPhysicalTypes[1] = "patch";
+    boundaryPatchPhysicalTypes[2] = "wall";
+    boundaryPatchPhysicalTypes[3] = "symmetry";
+
+    // name of the mesh region - use OF default
     word regionName = polyMesh::defaultRegion;
+
+    // default values for undefined faces
     word defaultFacesName = "defaultFaces";
     word defaultFacesType = emptyPolyPatch::typeName;
+
     Info << "Created physical and default values" << endl;
 
     // create the mesh
@@ -154,129 +248,9 @@ int main(int argc, char *argv[])
     Info << nl << "Writing extruded mesh to time = " << runTime.timeName() << nl << endl;
     mesh.write();
 
-/*
-	// These two create the time system (instance called runTime) and fvMesh (instance called mesh).
-    #include "createTime.H"
-    #include "createMesh.H"
+    // ---
+    Info << nl << "To best visualise the results, load the mesh and extract all patches" << nl << endl;
 
-	// runTime and mesh are instances of objects (or classes).
-	// If you are not familiar with what a class or object is, it is HIGHLY RECOMMENDED you visit this
-	// website and only come back once you've read everything about classes, inheritance and polymorphism:
-	// http://www.cplusplus.com/doc/tutorial/classes/
-	// Note how the next lines call functions .timeName(), .C() and .Cf() implemented in the objects.
-	// It is also important to realise that mesh.C() and .Cf() return vector fields denoting centres of each
-    // cell and internal face.
-	// Calling the mesh.C().size() method therefore yields the total size of the mesh.
-	Info << "Hello there, the most recent time folder found is " << runTime.timeName() << nl
-		 << "The mesh has " << mesh.C().size() << " cells and " << mesh.Cf().size()
-         << " internal faces in it. Wubalubadubdub!" << nl << endl;
-
-    // It's possible to iterate over every cell in a standard C++ for loop
-    for (label cellI = 0; cellI < mesh.C().size(); cellI++)
-        if (cellI%20 == 0) // only show every twentieth cell not to spam the screen too much
-            Info << "Cell " << cellI << " with centre at " << mesh.C()[cellI] << endl;
-    Info << endl; // spacer
-
-    // Each cell is constructed of faces - these may either be internal or constitute a
-    // boundary, or a patch in OpenFOAM terms; internal faces have an owner cell
-    // and a neighbour.
-    for (label faceI = 0; faceI < mesh.owner().size(); faceI++)
-        if (faceI%40 == 0)
-            Info << "Internal face " << faceI << " with centre at " << mesh.Cf()[faceI]
-                 << " with owner cell " << mesh.owner()[faceI]
-                 << " and neighbour " << mesh.neighbour()[faceI] << endl;
-    Info << endl;
-
-    // Boundary conditions may be accessed through the boundaryMesh object.
-    // In reality, each boundary face is also included in the constant/polyMesh/faces
-    // description. But, in that file, the internal faces are defined first.
-    // In addition, the constant/polyMesh/boundary file defines the starting faceI
-    // indices from which boundary face definitions start.
-    // OpenFOAM also provides a macro definition for for loops over all entries
-    // in a field or a list, which saves up on the amount of typing.
-    forAll(mesh.boundaryMesh(), patchI)
-        Info << "Patch " << patchI << ": " << mesh.boundary()[patchI].name() << " with "
-             << mesh.boundary()[patchI].Cf().size() << " faces. Starts at total face "
-             << mesh.boundary()[patchI].start() << endl;
-    Info << endl;
-
-    // Faces adjacent to boundaries may be accessed as follows.
-    // Also, a useful thing to know about a face is its normal vector and face area.
-    label patchFaceI(0);
-    forAll(mesh.boundaryMesh(), patchI)
-        Info << "Patch " << patchI << " has its face " << patchFaceI << " adjacent to cell "
-             << mesh.boundary()[patchI].patch().faceCells()[patchFaceI]
-             << ". It has normal vector " << mesh.boundary()[patchI].Sf()[patchFaceI]
-             << " and surface area " << mag(mesh.boundary()[patchI].Sf()[patchFaceI])
-             << endl;
-    Info << endl;
-
-    // For internal faces, method .Sf() can be called directly on the mesh instance.
-    // Moreover, there is a shorthand method .magSf() which returns the surface area
-    // as a scalar.
-    // For internal faces, the normal vector points from the owner to the neighbour
-    // and the owner has a smaller cellI index than the neighbour. For boundary faces,
-    // the normals always point outside of the domain (they have "imaginary" neighbours
-    // which do not exist).
-
-    // It is possible to look at the points making up each face in more detail.
-    // First, we define a few shorthands by getting references to the respective
-    // objects in the mesh. These are defined as constants since we do not aim to
-    // alter the mesh in any way.
-    // NOTE: these lists refer to the physical definition of the mesh and thus
-    // include boundary faces. Use can be made of the mesh.boundary()[patchI].Cf().size()
-    // and mesh.boundary()[patchI].start() methods to check whether the face is internal
-    // or lies on a boundary.
-    const faceList& fcs = mesh.faces();
-    const List<point>& pts = mesh.points();
-    const List<point>& cents = mesh.faceCentres();
-
-    forAll(fcs,faceI)
-        if (faceI%80==0)
-        {
-            if (faceI<mesh.Cf().size())
-                Info << "Internal face ";
-            else
-            {
-                forAll(mesh.boundary(),patchI)
-                    if ((mesh.boundary()[patchI].start()<= faceI) &&
-                        (faceI < mesh.boundary()[patchI].start()+mesh.boundary()[patchI].Cf().size()))
-                    {
-                        Info << "Face on patch " << patchI << ", faceI ";
-                        break; // exit the forAll loop prematurely
-                    }
-            }
-
-            Info << faceI << " with centre at " << cents[faceI]
-                 << " has " << fcs[faceI].size() << " vertices:";
-            forAll(fcs[faceI],vertexI)
-                // Note how fcs[faceI] holds the indices of points whose coordinates
-                // are stored in the pts list.
-                Info << " " << pts[fcs[faceI][vertexI]];
-            Info << endl;
-        }
-    Info << endl;
-
-    // In the original cavity tutorial, on which the test case is based,
-    // the frontAndBack boundary is defined as and "empty" type. This is a special
-    // BC case which may cause unexpected behaviour as its .Cf() field has size of 0.
-    // Type of a patch may be checked to avoid running into this problem if there
-    // is a substantial risk that an empty patch type will appear
-    label patchID(0);
-    const polyPatch& pp = mesh.boundaryMesh()[patchID];
-    if (isA<emptyPolyPatch>(pp))
-    {
-        // patch patchID is of type "empty".
-        Info << "You will not see this." << endl;
-    }
-
-    // Patches may also be retrieved from the mesh using their name. This could be
-    // useful if the user were to refer to a particular patch from a dictionary
-    // (like when you do when calculating forces on a particular patch).
-    word patchName("movingWall");
-    patchID = mesh.boundaryMesh().findPatchID(patchName);
-    Info << "Retrieved patch " << patchName << " at index " << patchID << " using its name only." << nl << endl;
-*/
     Info<< "End\n" << endl;
 
     return 0;
